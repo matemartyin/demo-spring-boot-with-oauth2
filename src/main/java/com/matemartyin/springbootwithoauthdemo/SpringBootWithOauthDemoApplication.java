a264b20f4d4ed1579e0d889df572f08b879024e7
matemartyin <matemartyin@gmail.com>
2022-06-23 18:16:30 +0200

    Init Spring Boot project

---
+++ src/main/java/com/matemartyin/springbootwithoauthdemo/SpringBootWithOauthDemoApplication.java
@@ -0,0 +1,13 @@
+package com.matemartyin.springbootwithoauthdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithOauthDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithOauthDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matemartyin/springbootwithoauthdemo/SpringBootWithOauthDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matemartyin.springbootwithoauthdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithOauthDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
